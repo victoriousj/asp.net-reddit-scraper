8bb88f8de6ddcf38d2f9620208ab63d3a9b4e94d
Vic <victordjohnson@gmail.com>
2019-02-01 06:50:03 -0600

    added error handling for 404 subreddits and a helper function for getting y/n answer from user

--- ReditScraper/Program.cs
+++ ReditScraper/Program.cs
@@ -23,6 +23,22 @@ namespace RedditScraper
 
 		public static void Main()
 		{
+			Intro();
+			GetUserInput();
+			DownloadRedditPosts();
+			DeleteBadFiles();
+			ConvertVideos();
+
+			Show(new[] {
+				"All steps complete...",
+				$"Go to \"{_directory}\" to see your files. Enjoy!"
+			});
+			Console.Beep();
+			Console.ReadLine();
+		}
+
+		public static void Intro()
+		{
 			Console.Title = "Reddit Scraper";
 			Show(new[]
 			{
@@ -35,14 +51,14 @@ namespace RedditScraper
                                                       | |              
    by: victor d. johnson                              |_|              
    this code is under MIT licence",
-				string.Empty,
-				@"Enter the name of a subreddit (without spaces) and specify an amount of",
-				@"of photos and a time frame. This program will download an image from each",
-				@"post found on the subreddit and place the image in a directory on your ",
-				@"computer. This will then try to clean up broken files and convert web ",
-				@"movies into video files that can be played locally.",
-				string.Empty
-			});
+							string.Empty,
+							@"Enter the name of a subreddit (without spaces) and specify an amount of",
+							@"of photos and a time frame. This program will download an image from each",
+							@"post found on the subreddit and place the image in a directory on your ",
+							@"computer. This will then try to clean up broken files and convert web ",
+							@"movies into video files that can be played locally.",
+							string.Empty
+						});
 
 			if (!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
 			{
@@ -51,32 +67,19 @@ namespace RedditScraper
 				Console.ReadLine();
 				return;
 			}
-
-
-			GetUserInput();
-			DownloadRedditPosts();
-			DeleteBadFiles();
-			ConvertVideos();
-
-			Show(new[] {
-				"All steps complete…",
-				$"Go to \"{_directory}\" to see your files. Enjoy!"
-			});
-			Console.Beep();
-			Console.ReadLine();
 		}
 
 		private static void GetUserInput()
 		{
-			Show(new[] { "Which subreddit would you like to scrape? (default \"funny\")" });
-			_inputSubreddit = GetInput("subreddit", "funny");
+			Show(new[] { "Which subreddit would you like to scrape? [funny]" });
+			_inputSubreddit = GetInput("subreddit: ", "funny");
 
-			Show(new[] { "How many posts would you like to try to download? (default 25)" });
-			if (!int.TryParse(GetInput("amount", "25"), out _amount)) _amount = 25;
+			Show(new[] { "How many posts would you like to try to download? [25]" });
+			if (!int.TryParse(GetInput("amount: ", "25"), out _amount)) _amount = 25;
 
 			Show(new[]
 			{
-				"Time Period? (default \"All Time\")",
+				"Time Period? [All Time]",
 				"0 = All Time",
 				"1 = Past Year",
 				"2 = Past Month",
@@ -84,45 +87,62 @@ namespace RedditScraper
 				"4 = Past Day",
 				"5 = Past Hour",
 			});
-			if (!Enum.TryParse(GetInput("from", "0"), out _time)) _time = FromTime.All;
+			if (Enum.TryParse(GetInput("from: ", "0"), out _time))
+			{
+				Console.SetCursorPosition(6, Console.CursorTop - 2);
+			}
+			Console.WriteLine(_time);
+			Console.WriteLine();
 		}
 
 		private static void DownloadRedditPosts()
 		{
-			_directory = $@"C:\reddit\{_inputSubreddit}\";
-			Show(new[] { $"Creating directory at \"{_directory}\"", string.Empty });
-
-			Show(new[] { $"Looking on {_subreddit} for {_amount} posts…", string.Empty });
-			_subreddit = new Reddit().GetSubreddit($"/r/{_inputSubreddit}");
+			try
+			{
+				_subreddit = new Reddit().GetSubreddit($"/r/{_inputSubreddit}");
+			} catch (WebException ex)
+			{
+				Show(new[] { "404: subreddit not found", string.Empty });
+			}
 
 			if (_subreddit == null)
 			{
 				Show(new[]
 				{
-					$"/r/{_inputSubreddit} doesn't appear to be a subreddit…",
-					"Try again…",
+					$"/r/{_inputSubreddit} doesn't appear to be a subreddit...",
+					"Try again...",
 					string.Empty
 				});
 				GetUserInput();
+				DownloadRedditPosts();
 			}
 
+			Show(new[] { $"Looking on {_subreddit} for {_amount} posts...", string.Empty });
 			var foundPosts = _subreddit
 				.GetTop(_time)
 				.Take(_amount)
 				.Select(x => x.Url.ToString());
-
 			Show(new[] { $"Found {foundPosts.Count()} posts on {_subreddit}", string.Empty });
+
+			_directory = $@"C:\reddit\{_inputSubreddit}\";
+			Show(new[] { $"Creating directory at \"{_directory}\"", string.Empty });
 
 			Directory.CreateDirectory(_directory);
 			System.Diagnostics.Process.Start(_directory);
 
+			Show(new[] { $"Downloading files from {_subreddit}" });
 			foreach(var foundPost in foundPosts)
 			{
 				DownloadImage(foundPost);
 			}
 
 			var files = Directory.GetFiles(_directory);
-			Show(new[] { "\n", $"Downloaded {files.Length} files from  {_subreddit}…", string.Empty });
+			Show(new[] 
+			{
+				string.Empty,
+				$"Downloaded {files.Length} files from  {_subreddit}...",
+				string.Empty
+			});
 		}
 
 		private static void DownloadImage(string imageURL)
@@ -181,20 +201,16 @@ namespace RedditScraper
 
 		private static void DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
 		{
-			lock (e.UserState) { Monitor.Pulse(e.UserState);}
+			lock (e.UserState)
+			{
+				Monitor.Pulse(e.UserState);
+			}
+			_magOrCy = !_magOrCy;
 		}
 
 		private static void DeleteBadFiles()
 		{
-			Show(new[]
-			{
-				"Attempt to clean up potionally corrupted files? (default yes)",
-				"1 = Yes",
-				"2 = No"
-			});
-
-			if (!int.TryParse(GetInput("clean", "1"), out int answer)) answer = 1;
-			if (answer != 1) return;
+			if (!Confirm(new[] { "Attempt to clean up potionally corrupted files? [Y/n]"})) return;
 
 			int badExtensionFiles = 0;
 			int removedFiles = 0;
@@ -229,12 +245,12 @@ namespace RedditScraper
 			}
 			Show(new[]
 			{
-				$"{brokenFiles} were improperly downloaded…",
-				$"{removedFiles} are no longer availible…",
-				$"{badExtensionFiles} have an unknown file extension…",
+				$"{brokenFiles} were improperly downloaded...",
+				$"{removedFiles} are no longer availible...",
+				$"{badExtensionFiles} have an unknown file extension...",
 				string.Empty,
-				$"{deletedFiles} files have been deleted overall…",
-				$"{files.Length - deletedFiles} remain from {_subreddit}…",
+				$"{deletedFiles} files have been deleted overall...",
+				$"{files.Length - deletedFiles} remain from {_subreddit}...",
 				string.Empty,
 			});
 		}
@@ -243,29 +259,29 @@ namespace RedditScraper
 			var files = Directory.GetFiles(_directory).Where(x => x.Contains("webm"));
 			if (files.Any())
 			{
-				Show(new[]
+				if (!Confirm(new[]
 				{
 					"It appears there were some .webm files downloaded. These can be hard",
 					"to play. Would you like to convert these videos? Short videos will be",
-					"converted to .gifs and longer ones will be .mp4 (default Yes)",
-					"1 = Yes",
-					"2 = No"
-				});
+					"converted to .gifs and longer ones will be .mp4 [Y/n]",
+				})) return;
 
-				if (!int.TryParse(GetInput("convert", "1"), out int answer)) answer = 1;
-				if (answer != 1) return;
-
-				Show(new[] { "Converting videos… This may take a couple of minutes…" });
-				int convertedVideoCount = 1;
 				int gifCount = 0;
 				int mp4Count = 0;
+				int convertedVideoCount = 1;
 				var ffMpeg = new FFMpegConverter();
+				files = files.OrderBy(x => int.Parse(Path.GetFileName(x.Split('-')[0])));
+				Show(new[] { "Converting videos... This may take a couple of minutes...", string.Empty });
 				foreach (var file in files)
 				{
-					Console.ForegroundColor = ConsoleColor.Magenta;
+					if (convertedVideoCount % 5 == 0)
+					{
+						_magOrCy = !_magOrCy;
+					}
+					Console.ForegroundColor = _magOrCy ? ConsoleColor.Magenta : ConsoleColor.Cyan;
 					Console.Write($"({convertedVideoCount} of {files.Count()}) Converting: ");
 					Console.ForegroundColor = ConsoleColor.White;
-					Console.Write(Path.GetFileName(file));
+					Console.Write(Path.GetFileName(file) + "...");
 					Console.WriteLine();
 
 					var fileInfo = new FileInfo(file);
@@ -279,11 +295,13 @@ namespace RedditScraper
 					fileInfo.Delete();
 					convertedVideoCount++;
 				}
-				Show(new[] 
+				Show(new[]
 				{
-					"All videos have been converted…",
-					$"{gifCount} gif's have been created…",
-					$"{mp4Count}.mp4's have been created…"
+					string.Empty,
+					"All videos have been converted...",
+					$"{gifCount} gif's have been created...",
+					$"{mp4Count}.mp4's have been created...",
+					string.Empty
 				});
 
 				new FileInfo("ffmpeg.exe").Delete();
@@ -292,10 +310,11 @@ namespace RedditScraper
 		}
 		private static string GetInput(string prompt, string defaultAnswer)
 		{
-			Console.Write(prompt + ": ");
+			Console.Write(prompt);
 			var input = Console.ReadLine();
 			if (string.IsNullOrWhiteSpace(input))
 			{
+				Console.SetCursorPosition(prompt.Length, Console.CursorTop - 1);
 				Console.Write(defaultAnswer);
 				Console.WriteLine();
 				Console.WriteLine();
@@ -307,21 +326,33 @@ namespace RedditScraper
 
 		private static void Show(string[] texts)
 		{
-			if (_magOrCy)
-			{
-				_magOrCy = false;
-				Console.ForegroundColor = ConsoleColor.Magenta;
-			}
-			else
-			{
-				_magOrCy = true;
-				Console.ForegroundColor = ConsoleColor.Cyan;
-			}
-			foreach(var text in texts)
+			Console.ForegroundColor = _magOrCy 
+				? ConsoleColor.Magenta 
+				: ConsoleColor.Cyan;
+			_magOrCy = !_magOrCy;
+
+			foreach (var text in texts)
 			{
 				Console.WriteLine(text);
 			}
 			Console.ForegroundColor = ConsoleColor.White;
+		}
+
+		public static bool Confirm(string[] prompt)
+		{
+			ConsoleKey res;
+			do
+			{
+				Show(prompt);
+				res = Console.ReadKey(false).Key;
+				if (res != ConsoleKey.Enter)
+				{
+					Console.WriteLine();
+				}
+			} while (res != ConsoleKey.Y && res != ConsoleKey.N && res != ConsoleKey.Enter);
+
+			Console.WriteLine();
+			return (res == ConsoleKey.Y || res == ConsoleKey.Enter);
 		}
 	}
 }
