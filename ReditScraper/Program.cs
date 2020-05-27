d71205533e762ef6deafd70e2ada0002a772322e
vic <victordjohnson@gmail.com>
2020-05-27 13:05:09 -0500

    ensure file extension is working and added null check to latest file retreival

--- ReditScraper/Program.cs
+++ ReditScraper/Program.cs
@@ -9,36 +9,37 @@ namespace RedditScraper
 {
     class Program
 	{
-		private static readonly string _imgurApiKey = File.Exists(AppDomain.CurrentDomain.BaseDirectory + "imgur-api-key.txt") ? File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "imgur-api-key.txt") : "";
-
-		private static void Main(string[] args)
+        #region Files
+        private static readonly string _rootDirectory = AppDomain.CurrentDomain.BaseDirectory;
+		private static readonly string _imgurApiKey = File.Exists(_rootDirectory + "imgur-api-key.txt") ? File.ReadAllText(_rootDirectory + "imgur-api-key.txt") : "";
+		private static readonly string _destination = File.Exists(_rootDirectory + "destination.txt") ? File.ReadAllText(_rootDirectory + "destination.txt") : Environment.ExpandEnvironmentVariables(@"%userprofile%\Downloads\RedditScraper");
+		private static readonly string[] _subreddits = File.Exists(_rootDirectory + "subreddits.txt") ? File.ReadAllLines(_rootDirectory + "subreddits.txt") : new []{ "EarthPorn", "FoodPorn", "HumanPorn", "OldSchoolCool" };
+		#endregion
+        private static void Main(string[] args)
 		{
             DownloadRedditPosts();
 		}
 
 		private static void DownloadRedditPosts()
 		{
-			var subreddits = File.ReadLines(AppDomain.CurrentDomain.BaseDirectory + "subreddits.txt");
-			foreach (var subreddit in subreddits)
-			{
-				var directory = $@"D:\Documents\Back-up\documents\abc\p\{subreddit}\";
-				var subredditPage = new Reddit().GetSubreddit($"/r/{subreddit}") ?? throw new WebException();
-				var redditPosts = subredditPage
-					.GetTop(RedditSharp.Things.FromTime.Day)
-					.ToList()
-					.Take(1)
-					.Select(x => (url: x.Url.ToString(), title: x.Title)).ToList();
+            foreach (var subreddit in _subreddits)
+            {
+                var directory = $@"{_destination}\{subreddit}\";
+                var subredditPage = new Reddit().GetSubreddit($"/r/{subreddit}") ?? throw new WebException();
+                var post = subredditPage.GetTop(RedditSharp.Things.FromTime.Day).Select(x => (url: x.Url.ToString(), title: x.Title)).FirstOrDefault();
 
-				Directory.CreateDirectory(directory);
-				redditPosts.ForEach(post => DownloadImage(post, directory));
-				DeleteBadFile(directory);
-			}
-		}
+                Directory.CreateDirectory(directory);
+                DownloadImage(post, directory);
+                DeleteBadFile(directory);
+            }
+        }
 
 		private static void DeleteBadFile(string directory)
 		{
-			var file = new DirectoryInfo(directory).GetFiles().OrderByDescending(f => f.LastWriteTime).FirstOrDefault();
-			if (file.Length == 0 || file.Length == 503 || string.IsNullOrWhiteSpace(file.Extension) || file.Extension.Contains("com"))
+			var fileTypes = new []{ ".JPG", ".JPE", ".BMP", ".GIF", ".PNG", ".MP4" };
+
+            var file = new DirectoryInfo(directory).GetFiles().OrderByDescending(f => f.LastWriteTime).FirstOrDefault();
+            if (file != null && file.Length == 0 || file.Length == 503 || file.Length == 9236 || !fileTypes.Contains(file.Extension.ToUpper()))
 			{
 				file.Delete();
 			}
@@ -48,59 +49,47 @@ namespace RedditScraper
 		{
 			var (url, fileName) = post;
 			
-			FixImageUrl(ref url, false);
-			FixFileName(ref fileName, ref url);
+			FixFileAndURL(ref fileName, ref url);
 
 			var lastFile = new DirectoryInfo(directory).GetFiles().OrderByDescending(f => f.LastWriteTime).FirstOrDefault();
-			if (lastFile.Name.Contains(fileName.Substring(4)) || string.IsNullOrWhiteSpace(url))
-			{
-				return;
-			}
+			if (lastFile != null && lastFile.Name.Contains(fileName.Substring(4)) || string.IsNullOrWhiteSpace(url)) return;
 
 			string path = directory + fileName;
-
 			using (var wc = new WebClient())
 			{
-				wc.DownloadFile(new Uri(url), path);
+				try { wc.DownloadFile(new Uri(url), path); } catch { }
 			}
 		}
 
-		private static void FixFileName(ref string fileName, ref string url)
+		private static void FixFileAndURL(ref string fileName, ref string url)
 		{
+			var daysSince = 1000 - Math.Floor((DateTime.Now - new DateTime(2020, 1, 1)).TotalDays);
 			string fileExtension = url.Split('.').Last();
-			if (fileExtension.Contains("com") && url.Contains("imgur.com"))
-            {
-				FixImageUrl(ref url, true);
-				fileExtension = url.Split('.').Last();
-			}
-			else if (fileExtension.Length > 3)
-			{ 
-				fileExtension = fileExtension.Substring(0, 3);
-			}
+			FixImageUrl
+			(
+				url : ref url, 
+				imgur: fileExtension.Contains("com") && url.Contains("imgur.com"), 
+				transfer : fileExtension.Contains("com") && url.Contains("gfycat.com")
+			);
+			fileExtension = url.Split('.').Last();
 
 			fileName = WebUtility.HtmlEncode(fileName);
-			fileName = Regex.Replace(fileName, @"&#[0-9]{5,};", "");
+			fileName = Regex.Replace(fileName, @"&#[0-9]{5,};", "").Replace(@"\s+", " ");
 			fileName = WebUtility.HtmlDecode(fileName);
-			fileName = fileName.Replace("&amp;", "&").Replace("&lt;3", "♡").Replace("&gt;","");
-			fileName = Regex.Replace(fileName, @"\s{2,}", " ").Trim();
+			fileName = Regex.Replace(fileName, "&amp;", "&").Replace("&lt;3", "♡").Replace("&gt;","");
 			fileName = fileName.Substring(0, Math.Min(225, fileName.Length));
+			fileName = string.IsNullOrWhiteSpace(fileName) ? "" : $" {fileName.Replace(".", "").Trim()}";
+			fileName = string.Concat(fileName.Split(Path.GetInvalidFileNameChars()));
 
-			var daysSince = 1000 - Math.Floor((DateTime.Now - new DateTime(2020, 1, 1)).TotalDays);
-
-			fileName = $"{daysSince}{" " + fileName.Replace(".", "").Trim()}.{fileExtension}";
-
-			foreach(char c in Path.GetInvalidFileNameChars())
-			{
-				fileName = fileName.Replace(c.ToString(), "");
-			}
+			fileName = $"{daysSince}{fileName}.{fileExtension}";
 		}
 
-		private static void FixImageUrl(ref string url, bool imgur)
+		private static void FixImageUrl(ref string url, bool imgur, bool transfer)
 		{
-            (string url, string parent, string child) parameters;
-            switch (url)
-            {
-                case string post when post.Contains("redgifs.com"):
+			(string url, string parent, string child) parameters;
+			switch (url)
+			{
+				case string post when post.Contains("redgifs.com") || transfer:
                     parameters = (@"https://api.redgifs.com/v1/gfycats/", "gfyItem", "mp4Url");
                     break;
                 case string post when post.Contains(".gifv"):
@@ -118,10 +107,7 @@ namespace RedditScraper
             try
 			{
 				var request = WebRequest.Create(parameters.url + url.Split('/').Last());
-				if (imgur)
-				{
-					request.Headers.Add("Authorization", _imgurApiKey);
-				}
+				if (imgur) request.Headers.Add("Authorization", _imgurApiKey);
 
 				using (WebResponse response = request.GetResponse())
 				using (Stream responseStream = response.GetResponseStream())
